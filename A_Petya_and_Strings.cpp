1119fa20c77b26b3989cf59701593fa0b20b527f
Ratnakar-7 <105923165+Ratnakar-7@users.noreply.github.com>
2023-04-15 08:48:42 +0530

    cf-800

---
+++ A_Petya_and_Strings.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string a, b;
+    cin >> a >> b;
+    transform(a.begin(), a.end(), a.begin(), ::tolower);
+    transform(b.begin(), b.end(), b.begin(), ::tolower);
+
+    if (a.size() == b.size())
+        for (int x = 0; x < a.size(); x++)
+        {
+            if (a == b)
+            {
+                cout << "0" << endl;
+                break;
+            }
+            if (a[x] > b[x])
+            {
+                cout << "1" << endl;
+                break;
+            }
+            if (a[x] < b[x])
+            {
+                cout << "-1" << endl;
+                break;
+            }
+        }
+}
